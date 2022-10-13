e716c7613e5a127ff7de0d519fa2dab19637e615
4ssef <fernandoassef@hotmail.com>
2022-10-13 15:53:06 -0500

    chore: crea setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+	name="powerpy",
+	version="0.1.0",
+	description="Librería para comunicación de Power BI REST API y cliente",
+    url="https://github.com/4ssef/powerpy_package",
+    author="4ssef (Fernando Assef)",
+    author_email="fernandoassef@hotmail.com",
+    license="MIT License",
+    packages=find_packages(),
+    install_requires=[],
+	keywords=['python', 'power bi', 'power bi rest api', 'powerbi']
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.9',
+		'Operating System :: OS Independent',
+		'Intended Audience :: Information Technology',
+		'License :: OSI Approved :: MIT License',
+		'Natural Language :: English',
+		'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
